76bc00c2036c725f4aa5de06a8f148bf22b6f997
kaly diallo <kaly100diallo@gmail.com>
2024-09-26 10:14:00 +0100

    cors url-||-localhost

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,7 @@ const app = express();
         cookie: { secure: false, maxAge: 1000 * 60 * 60 * 24 }
     }));
     app.use(cors({
-        origin : ["http://localhost:3000", "https://enens.vercel.app/"] ,
+        origin : process.env.FRONT_URL || "locahost://3000",
         methods : ["GET","POST","PUT","DELETE"],
         credentials : true
     }));
